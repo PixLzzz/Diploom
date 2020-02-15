99244c3c87d1321dc6aa23644e0c86570d9ee7ab
Louis Cazenave <louisc92500@yahoo.fr>
2020-02-15 22:07:29 +0100

    add main menu

--- front-diploom/src/app/app-routing.module.ts
+++ front-diploom/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SchoolComponent } from './school/school.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component : HomeComponent},
+    {path:'school' , component : SchoolComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-diploom/src/app/app.module.ts
+++ front-diploom/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { SchoolComponent } from './school/school.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
