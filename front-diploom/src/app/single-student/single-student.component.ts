349fa56c5f459236ad9acbed9c037fc6965ba2f8
Louis Cazenave <louisc92500@yahoo.fr>
2020-05-02 02:07:17 +0200

    new single student and diploma dl

--- front-diploom/src/app/single-student/single-student.component.ts
+++ front-diploom/src/app/single-student/single-student.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/student.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import firebase from 'firebase';
+import { DialogComponent } from '../dialog/dialog.component';
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-single-student',
@@ -7,9 +14,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleStudentComponent implements OnInit {
 
-  constructor() { }
+  student: Student;
+  constructor(private route: ActivatedRoute, private studentService: StudentService,
+              private router: Router,public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.student = new Student();
+    const id = this.route.snapshot.params['id'];
+    this.studentService.getSingleStudent(+id).then(
+      (student: Student) => {
+        this.student = student;
+      }
+    );
   }
+
+  onBack() {
+    this.router.navigate(['schoolHome']);
+  }
+
+
 
 }
